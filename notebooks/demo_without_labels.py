848b3ff6f8efa8b9fcee61e09f4e9708033d57fd
vasikoro <vasi_koro@hotmail.com>
2021-06-21 11:47:42 +0300

    add convert np.array to Tensor error :    ValueError: Failed to convert a NumPy array to a Tensor

--- notebooks/demo_without_labels.py
+++ notebooks/demo_without_labels.py
@@ -81,6 +81,7 @@ if __name__ == '__main__':
     print(df_test.head())
     print(df_train.shape, df_test.shape)
 
+    TIME_STEPS = 3
 
     def create_dataset(X, y, time_steps=1):
         Xs, ys = [], []
@@ -91,22 +92,30 @@ if __name__ == '__main__':
         return np.array(Xs), np.array(ys)
 
 
-    TIME_STEPS = 30
+    print(train[['use_to_train']].shape, df_train.T.shape)
     # reshape to [samples, time_steps, n_features]
     X_train, y_train = create_dataset(
-        df_train.T.T, train[['use_to_train']], TIME_STEPS
+        train[['use_to_train']], df_train.T, TIME_STEPS
     )
 
     X_test, y_test = create_dataset(
-        df_test.T.T, test[['use_to_test']], TIME_STEPS
+        test[['use_to_test']], df_test.T, TIME_STEPS
     )
     # couldn't add timestamps
     print(X_train.shape, X_test.shape)
+    print(X_train[0])
+    print(train[['use_to_train']].shape, df_train.T.shape)
+
+    X_train_tensor = tf.convert_to_tensor(np.asarray(X_train).astype(np.float32))
+    X_test_tensor = tf.convert_to_tensor(np.asarray(X_test).astype(np.float32))
+
+    Y_train_tensor = tf.convert_to_tensor(np.asarray(y_train).astype(np.float32))
+    Y_test_tensor = tf.convert_to_tensor(np.asarray(y_test).astype(np.float32))
 
     model = keras.Sequential()
     model.add(keras.layers.LSTM(
         units=64,
-        input_shape=(X_train.shape[1], X_train.shape[2])
+        input_shape=(X_train_tensor.shape[1], X_train_tensor.shape[2])
     ))
     model.add(keras.layers.Dropout(rate=0.2))
     model.add(keras.layers.RepeatVector(n=X_train.shape[1]))
@@ -114,7 +123,7 @@ if __name__ == '__main__':
     model.add(keras.layers.Dropout(rate=0.2))
     model.add(
         keras.layers.TimeDistributed(
-            keras.layers.Dense(units=X_train.shape[2])
+            keras.layers.Dense(units=X_train_tensor.shape[2])
         )
     )
     model.compile(loss='mae', optimizer='adam')
@@ -122,19 +131,19 @@ if __name__ == '__main__':
     print(model.summary())
 
     history = model.fit(
-        X_train, y_train,
+        X_train_tensor, Y_train_tensor,
         epochs=10,
         batch_size=32,
         validation_split=0.1,
         shuffle=False
     )
 
-    X_train_pred = model.predict(X_train)
-    train_mae_loss = np.mean(np.abs(X_train_pred - X_train), axis=1)
+    X_train_pred = model.predict(X_train_tensor)
+    train_mae_loss = np.mean(np.abs(X_train_pred - X_train_tensor), axis=1)
 
     THRESHOLD = 0.65
-    X_test_pred = model.predict(X_test)
-    test_mae_loss = np.mean(np.abs(X_test_pred - X_test), axis=1)
+    X_test_pred = model.predict(X_test_tensor)
+    test_mae_loss = np.mean(np.abs(X_test_pred - X_test_tensor), axis=1)
     test_score_df = pd.DataFrame(index=test[TIME_STEPS:].index)
     test_score_df['loss'] = test_mae_loss
     test_score_df['threshold'] = THRESHOLD
